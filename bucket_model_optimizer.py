7bb1827dc2845c04bddeb2443b02c7525daef9b0
“CooperBigFoot” <nlazaro@student.ethz.ch>
2024-03-18 08:37:58 +0100

    initial commit

---
+++ bucket_model_optimizer.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from scipy.optimize import minimize, basinhopping
+import numpy as np
+from dataclasses import dataclass, field
+from bucket_model import BucketModel
+
+
+@dataclass
+class BucketModelOptimizer:
+    model: BucketModel
+    training_data: pd.DataFrame
+    method: str = field(init=False, repr=False)
+    bounds: dict = field(init=False, repr=False)
+
+    # You should define some other Goodness of Fit metrics here too (e.g. log_nse, mae, pbias). I only included rmse as an example.
+    def rmse(self, simulated_Q: pd.DataFrame, observed_Q: pd.Series) -> float:
+        """Calculate the Root Mean Squared Error (RMSE) between observed and simulated Q values.
+
+        Parameters:
+        observed_Q (np.ndarray): Array of observed Q values.
+        simulated_Q (np.ndarray): Array of simulated Q values.
+
+        Returns:
+        float: The RMSE value.
+        """
+        squared_errors = (observed_Q - simulated_Q) ** 2
+        mse_value = np.mean(squared_errors)
+        rmse_value = np.sqrt(mse_value)
+        return rmse_value
+
+    def set_options(self, method: str, bounds: dict):
+        self.method = method
+        self.bounds = bounds
+
+    def _objective_function(self, params: np.ndarray) -> float:
+        """
+        This is a private method used to calculate the objective function, 
+        which in this case is the RMSE between observed and simulated Q values.
+
+        The params array contains the optimization variables, which need to be
+        mapped to the model's parameters.
+        """
+        # Map the params array to the model's parameters
+        param_dict = {key: value for key,
+                      value in zip(self.bounds.keys(), params)}
+        self.model.update_parameters(param_dict)
+
+        observed_Q = self.training_data['Q']
+        data = self.training_data.drop(columns='Q', axis=1)
+
+        model_run = self.model.run(data)
+        simulated_Q = model_run['Q_s'] + model_run['Q_gw']
+        return self.rmse(simulated_Q, observed_Q)
+
+    def optimize(self) -> dict:
+        """
+        This method optimizes the model's parameters using the method and bounds
+        specified in the set_options method. The method can be either 'local' or 'global'.
+
+        Returns:
+        dict: A dictionary containing the optimal parameters.
+        """
+
+        def create_param_dict(keys, values):
+            """This is a helper function that creates a dictionary from two lists."""
+
+            return {key: value for key, value in zip(keys, values)}
+        
+        # This is a list of tuples. Each tuple contains the lower and upper bounds for each parameter.
+        bounds_list = list(self.bounds.values())
+
+        # Randomly generate an initial guess for each parameter.
+        initial_guess = [np.random.uniform(lower, upper)
+                         for lower, upper in bounds_list]
+
+        if self.method == 'local':
+            method = 'L-BFGS-B'  # Have a look at the doc for more methods: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
+
+            # Use a lambda to pass 'self' to the objective function
+            result = minimize(
+                lambda params: self._objective_function(params),
+                initial_guess,
+                method=method,
+                bounds=bounds_list
+            )
+
+            optimal_param_list = result.x
+            optimal_param_dict = create_param_dict(self.bounds.keys(), optimal_param_list)
+
+            return optimal_param_dict
+        
+        elif self.method == 'global':
+            # The way basinhopping works, is that it find a bunch of local minima and then chooses the best one. Hence the name 'basinhopping'. 
+            # The method is slow (many hours). I do not recommend using it unless you have a good reason to do so. 
+            # You can try to tune the parameters of the method to make it faster: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.basinhopping.html
+            result = basinhopping(
+                lambda params: self._objective_function(params),
+                initial_guess,
+                minimizer_kwargs={'method': 'L-BFGS-B', 'bounds': bounds_list}
+            )
+
+            optimal_param_list = result.x
+            optimal_param_dict = create_param_dict(self.bounds.keys(), optimal_param_list)
+
+            return optimal_param_dict
---
+++ data_preprocessor.py
@@ -0,0 +1,50 @@
+import pandas as pd 
+
+def preprocess_data(path_to_file: str, output_destination: str, catchment_area: float) -> None:
+    """This function takes the .txt file you find on moodle and transforms it into a pandas DataFrame.
+    
+    Parameters:
+    - path_to_file (str): The path to the .txt file
+    - output_destination (str): The path to the new .csv file
+    - catchment_area (float): The catchment area in km^2
+    """
+
+    precipitation = pd.read_csv(path_to_file, sep='\s+', skiprows=1, header=0)
+
+    precipitation = precipitation.set_index('day')
+
+    # Create a DatetimeIndex starting from October 1st, 1985
+    date_range = pd.date_range(start='1985-10-01', periods=len(precipitation), freq='D')
+
+    # Set the DatetimeIndex as the new index of the DataFrame
+    precipitation.set_index(date_range, inplace=True)
+
+    # Rename index to 'date'
+    precipitation.index.name = 'date'
+
+    # Set date to datetime format
+    precipitation.index = pd.to_datetime(precipitation.index)
+    precipitation = precipitation.apply(pd.to_numeric, errors='coerce')
+
+    # Only keep data from 1986  to 1999
+    precipitation = precipitation.loc['1986':'2000']
+
+    precipitation['Q'] = (precipitation['Q'] * 60 * 60 * 24) / catchment_area / 1000 # Convert m^3/s to mm/day
+    
+    # Save the preprocessed data to a new .csv file
+    precipitation.to_csv(output_destination, index=True, header=True)
+
+def main() -> None:
+    """
+    This is an example of how you can use this function. You need to change the path_to_file and output_destination to your own paths.
+    Alternatively you can import this function into another script and use it there. See example_run.ipynb for more information.
+    """
+
+    path_to_file = '/Users/cooper/Desktop/BucketModel/data/GSTEIGmeteo.txt'
+    output_destination = '/Users/cooper/Desktop/BucketModel/data/GSTEIGmeteo.csv'
+    catchment_area = 384.2 # km^2
+    preprocess_data(path_to_file, output_destination, catchment_area)
+    
+if __name__ == "__main__":
+    main()
+
