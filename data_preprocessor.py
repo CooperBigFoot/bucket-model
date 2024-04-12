f5dbfdb24bb1cb012b3eb4c12b95fc1b908b4beb
“CooperBigFoot” <nlazaro@student.ethz.ch>
2024-04-12 16:53:44 +0200

    remove redundant index allocation in preprocess_data function

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -7,12 +7,9 @@ def preprocess_data(path_to_file: str, catchment_area: float, output_destination
     - path_to_file (str): The path to the .txt file
     - output_destination (str): The path to the new .csv file
     - catchment_area (float): The catchment area in km^2
-    - save (bool): If True, the preprocessed data will be saved to a new .csv file. If False, the function will only return the preprocessed data.
-    """
+  """
 
     precipitation = pd.read_csv(path_to_file, sep='\s+', skiprows=1, header=0)
-
-    precipitation = precipitation.set_index('day')
 
     # Create a DatetimeIndex starting from October 1st, 1985
     date_range = pd.date_range(start='1985-10-01', periods=len(precipitation), freq='D')
@@ -27,7 +24,7 @@ def preprocess_data(path_to_file: str, catchment_area: float, output_destination
     precipitation.index = pd.to_datetime(precipitation.index)
     precipitation = precipitation.apply(pd.to_numeric, errors='coerce')
 
-    # Only keep data from 1986  to 1999
+    # Only keep data from 1986  to 2000
     precipitation = precipitation.loc['1986':'2000']
 
     precipitation['Q'] = (precipitation['Q'] * 60 * 60 * 24) / catchment_area / 1000 # Convert m^3/s to mm/day
@@ -43,10 +40,12 @@ def main() -> None:
     Alternatively you can import this function into another script and use it there. See example_run.ipynb for more information.
     """
 
-    path_to_file = '/Users/cooper/Desktop/BucketModel/data/GSTEIGmeteo.txt'
-    output_destination = '/Users/cooper/Desktop/BucketModel/data/GSTEIGmeteo.csv'
+    path_to_file = '/Users/cooper/Desktop/bucket-model/data/GSTEIGmeteo.txt'
+    output_destination = '/Users/cooper/Desktop/bucket-model/data/GSTEIGmeteo.csv'
     catchment_area = 384.2 # km^2
-    preprocess_data(path_to_file, output_destination, catchment_area)
+    data = preprocess_data(path_to_file, output_destination, catchment_area)
+    print(data)
+    
     
 if __name__ == "__main__":
     main()
