d2d1eeedb08c2ee3d738ed3cadc726ee6a596a1d
“CooperBigFoot” <nlazaro@student.ethz.ch>
2024-05-07 18:52:09 +0200

    first gitlab commit

--- bucket_model_optimizer.py
+++ bucket_model_optimizer.py
@@ -3,7 +3,7 @@ from scipy.optimize import minimize, basinhopping
 import numpy as np
 from dataclasses import dataclass, field
 from bucket_model import BucketModel
-from metrics import rmse, nse, log_nse, mae, kge, pbias
+from metrics import nse, log_nse, mae, kge, pbias, rmse
 import concurrent.futures
 from typing import Union
 import matplotlib.pyplot as plt
@@ -11,16 +11,17 @@ import matplotlib.patches as mpatches
 
 # If you want to add a new metric, you need to implement it in metrics.py and add it to the GOF_DICT dictionary.
 GOF_DICT = {
-    'rmse': rmse,
-    'nse': nse,
-    'log_nse': log_nse,
-    'mae': mae,
-    'kge': kge,
-    'pbias': pbias
+    "rmse": rmse,
+    "nse": nse,
+    "log_nse": log_nse,
+    "mae": mae,
+    "kge": kge,
+    "pbias": pbias,
 }
 
+
 @dataclass
-class BucketModelOptimizer():
+class BucketModelOptimizer:
     model: BucketModel
     training_data: pd.DataFrame
     validation_data: pd.DataFrame = None
@@ -33,7 +34,6 @@ class BucketModelOptimizer():
     def create_param_dict(keys, values):
         """This is a helper function that creates a dictionary from two lists."""
         return {key: value for key, value in zip(keys, values)}
-    
 
     def set_options(self, method: str, bounds: dict, folds: int = 0) -> None:
         """
@@ -43,17 +43,19 @@ class BucketModelOptimizer():
         - method (str): The optimization method to use. Can be either 'local' or 'global'.
         - bounds (dict): A dictionary containing the lower and upper bounds for each parameter.
         """
-        possible_methods = ['local', 'n-folds', 'global']
+        possible_methods = ["local", "n-folds"]
 
         if method not in possible_methods:
             raise ValueError(f"Method must be one of {possible_methods}")
-        
+
+        if method == "n-folds" and folds == 1:
+            raise ValueError(
+                "You must provide the number of folds for the n-folds method."
+            )
+        self.folds = folds
+
         self.method = method
         self.bounds = bounds
-
-        if method == 'n-folds' and folds == 0:
-            raise ValueError("You must provide the number of folds for the n-folds method.")
-        self.folds = folds
 
     def _objective_function(self, params: list) -> float:
         """
@@ -73,37 +75,50 @@ class BucketModelOptimizer():
 
         results = model_copy.run(self.training_data)
 
-        simulated_Q = results['Q_s'] + results['Q_gw']
+        simulated_Q = results["Q_s"] + results["Q_gw"]
 
-        # Objective function is RMSE, minimized. Change metric if needed, adjust sign accordingly.
-        objective_function = rmse(simulated_Q, self.training_data['Q']) 
+        # Objective function is NSE, minimized. Change metric if needed, adjust sign accordingly.
+        objective_function = -nse(simulated_Q, self.training_data["Q"])
 
         return objective_function
-    
-    def single_fold_calibration(self, bounds_list: list[tuple], initial_guess: list[float] = None) -> list[float]:
+
+    def single_fold_calibration(
+        self, bounds_list: list[tuple], initial_guess: list[float] = None
+    ) -> list[float]:
         """Performs a single fold calibration using random initial guesses.
-        
+
         Parameters:
         - bounds_list (list[tuple]): A list of tuples containing the lower and upper bounds for each parameter.
         - initial_guess (list[float]): A list of initial guesses for the parameters"""
 
         if initial_guess is None:
-            initial_guess = [round(np.random.uniform(lower, upper), 3) for lower, upper in bounds_list] # Round to 3 decimal places
+            initial_guess = [
+                np.random.uniform(lower, upper) for lower, upper in bounds_list
+            ]
+
+            print(f"Initial guess: {initial_guess}")
+            print(f"Bounds: {bounds_list}")
 
         options = {
-                'eps': 1e-3, 
-            }
+            # 'ftol': 1e-8,
+            # 'gtol': 1e-5,
+            "eps": 1e-3,
+        }
+
+        def print_status(x):
+            print("Current parameter values:", x)
+
         result = minimize(
             lambda params: self._objective_function(params),
             initial_guess,
-            method='L-BFGS-B', # Have a look at the doc for more methods: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
+            method="L-BFGS-B",  # Have a look at the doc for more methods: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
             bounds=bounds_list,
-            options=options
+            options=options,
+            # callback=print_status, # Uncomment this line to print the current parameter values at each iteration
         )
         return [round(param, 3) for param in result.x]
 
-
-    def calibrate(self, initial_guess : list[float] = None) -> tuple[dict, pd.DataFrame]:
+    def calibrate(self, initial_guess: list[float] = None) -> tuple[dict, pd.DataFrame]:
         """
         This method calibrates the model's parameters using the method and bounds
         specified in the set_options method. The method can be either 'local', 'global' or 'n-folds'.
@@ -118,76 +133,49 @@ class BucketModelOptimizer():
         # This is a list of tuples. Each tuple contains the lower and upper bounds for each parameter.
         bounds_list = list(self.bounds.values())
 
-        # Randomly generate an initial guess for each parameter.
-        if self.method == 'local':
-
-            optimal_param_list = self.single_fold_calibration(bounds_list, initial_guess)
-            calibrated_parameters = BucketModelOptimizer.create_param_dict(self.bounds.keys(), optimal_param_list)
-
-            self.model.update_parameters(calibrated_parameters)
-
-            return calibrated_parameters, None
-        
-        elif self.method == 'n-folds':
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                n_fold_results = list(executor.map(self.single_fold_calibration, [bounds_list] * self.folds))
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            n_fold_results = list(
+                executor.map(
+                    self.single_fold_calibration, [bounds_list] * max(1, self.folds)
+                )
+            )
 
             columns = list(self.bounds.keys())
             n_fold_results = pd.DataFrame(n_fold_results, columns=columns)
-            # print(results)
 
             calibrated_parameters = self.get_best_parameters(n_fold_results)
 
             return calibrated_parameters, n_fold_results
 
-        # # TODO: Get rid of this?
-        # elif self.method == 'global':
-        #     # The way basinhopping works, is that it find a bunch of local minima and then chooses the best one. Hence the name 'basinhopping'. 
-        #     # The method is slow (many hours). I do not recommend using it unless you have a good reason to do so. 
-        #     # You can try to tune the parameters of the method to make it faster: https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.basinhopping.html
-        #     result = basinhopping(
-        #         lambda params: self._objective_function(params),
-        #         initial_guess,
-        #         minimizer_kwargs={'method': 'L-BFGS-B', 'bounds': bounds_list}
-        #     )
-
-        #     optimal_param_list = [round(param, 3) for param in result.x]
-        #     calibrated_parameters = BucketModelOptimizer.create_param_dict(self.bounds.keys(), optimal_param_list)
-
-        # # Update the model's parameters with the calibrated parameters. This is important for scoring the model.
-        # self.model.update_parameters(calibrated_parameters)
-
-        # return calibrated_parameters
-
     def get_best_parameters(self, results: pd.DataFrame) -> dict:
         """This function takes a DataFrame containing the results of the n-folds calibration and returns the one that performs best.
-        
+
         Parameters:
         - results (pd.DataFrame): A DataFrame containing the results of the n-folds calibration.
-        
+
         Returns:
         - dict: A dictionary containing the best parameters.
         """
-        best_rmse = float('inf')
+        best_nse = float("inf")
         best_parameters = None
 
         for index, row in results.iterrows():
             # Convert row to parameter dictionary
             params = row.to_dict()
-            
+
             self.model.update_parameters(params)
-            
+
             simulated_results = self.model.run(self.training_data)
-            
-            simulated_Q = simulated_results['Q_s'] + simulated_results['Q_gw']
-            observed_Q = self.training_data['Q']
-            
-            # Calculate RMSE
-            current_rmse = rmse(simulated_Q, observed_Q)
-            
-            # Check if the current RMSE is the best one
-            if current_rmse < best_rmse:
-                best_rmse = current_rmse
+
+            simulated_Q = simulated_results["Q_s"] + simulated_results["Q_gw"]
+            observed_Q = self.training_data["Q"]
+
+            # Calculate nse
+            current_nse = nse(simulated_Q, observed_Q)
+
+            # Check if the current nse is the best one
+            if current_nse < best_nse:
+                best_nse = current_nse
                 best_parameters = params
 
         # Update the model's parameters with the best parameters, otherwise the last set of parameters will be used.
@@ -195,36 +183,44 @@ class BucketModelOptimizer():
 
         return best_parameters
 
-    def score_model(self, metrics: list[str] = ['rmse']) -> dict:
+    def score_model(self, metrics: list[str] = ["nse"]) -> dict:
         """
         This function calculates the goodness of fit metrics for a given model.
 
         Parameters:
-        - metrics (list(str)): A list of strings containing the names of the metrics to calculate. If no metrics are provided, only RMSE is calculated.
+        - metrics (list(str)): A list of strings containing the names of the metrics to calculate. If no metrics are provided, only nse is calculated.
 
         Returns:
         - dict: A dictionary containing the scores for the training and validation data.
         """
 
-        metrics = [metric.lower() for metric in metrics] # Convert all metrics to lowercase
+        metrics = [
+            metric.lower() for metric in metrics
+        ]  # Convert all metrics to lowercase
 
         training_results = self.model.run(self.training_data)
-        simulated_Q = training_results['Q_s'] + training_results['Q_gw']
-        observed_Q = self.training_data['Q']
-        training_score = {metric: round(GOF_DICT[metric](simulated_Q, observed_Q), 3) for metric in metrics}
+        simulated_Q = training_results["Q_s"] + training_results["Q_gw"]
+        observed_Q = self.training_data["Q"]
+        training_score = {
+            metric: round(GOF_DICT[metric](simulated_Q, observed_Q), 3)
+            for metric in metrics
+        }
 
-        scores = {'training': training_score}
+        scores = {"training": training_score}
 
         if self.validation_data is not None:
             validation_results = self.model.run(self.validation_data)
-            simulated_Q = validation_results['Q_s'] + validation_results['Q_gw']
-            observed_Q = self.validation_data['Q']
-            validation_score = {metric: round(GOF_DICT[metric](simulated_Q, observed_Q), 3) for metric in metrics}
+            simulated_Q = validation_results["Q_s"] + validation_results["Q_gw"]
+            observed_Q = self.validation_data["Q"]
+            validation_score = {
+                metric: round(GOF_DICT[metric](simulated_Q, observed_Q), 3)
+                for metric in metrics
+            }
 
-            scores['validation'] = validation_score
+            scores["validation"] = validation_score
 
         return scores
-    
+
     # TODO: Add customization options after meeting with team
     def plot_of_surface(self, param1: str, param2: str, n_points: int) -> None:
         """
@@ -237,8 +233,12 @@ class BucketModelOptimizer():
         """
         params = self.model.get_parameters().copy()
         # print(params)
-        param1_values = np.linspace(self.bounds[param1][0], self.bounds[param1][1], n_points)
-        param2_values = np.linspace(self.bounds[param2][0], self.bounds[param2][1], n_points)
+        param1_values = np.linspace(
+            self.bounds[param1][0], self.bounds[param1][1], n_points
+        )
+        param2_values = np.linspace(
+            self.bounds[param2][0], self.bounds[param2][1], n_points
+        )
         PARAM1, PARAM2 = np.meshgrid(param1_values, param2_values)
 
         goal_matrix = np.zeros(PARAM1.shape)
@@ -255,22 +255,14 @@ class BucketModelOptimizer():
         plt.figure(figsize=(10, 7))
         levels = np.linspace(np.min(goal_matrix), np.max(goal_matrix), 20)
 
-        CP = plt.contour(PARAM1, PARAM2, goal_matrix, levels=levels, cmap='viridis')
+        CP = plt.contour(PARAM1, PARAM2, goal_matrix, levels=levels, cmap="viridis")
         plt.clabel(CP, inline=True, fontsize=10)
 
-        plt.xlabel(f'{param1} (mm/d/°C)')
-        plt.ylabel(f'{param2} (days)')
+        plt.xlabel(f"{param1} (mm/d/°C)")
+        plt.ylabel(f"{param2} (days)")
 
         # print(params)
 
-        plt.scatter(params[param1], params[param2], color='red', label='Optimal Point')
+        plt.scatter(params[param1], params[param2], color="red", label="Optimal Point")
         plt.legend()
         plt.show()
-
-
-
-
- 
-
-
-
